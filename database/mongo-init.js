62a5a9ad44107a4c570e37ee883d7f8c539e1c7e
Irina Nita <irinaanita18@gmail.com>
2024-05-09 15:38:16 +0300

    update docker compose
    Signed-off-by: Irina Nita <irinaanita18@gmail.com>

---
+++ database/mongo-init.js
@@ -0,0 +1,19 @@
+db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
+db.auth(process.env.MONGO_INITDB_ROOT_USERNAME, process.env.MONGO_INITDB_ROOT_PASSWORD);
+db = db.getSiblingDB('school');
+
+db.createUser({
+  user: process.env.MONGO_SCHOOL_USERNAME,
+  pwd:  process.env.MONGO_SCHOOL_PASSWORD,
+  roles: [
+    {
+      role: 'readWrite',
+      db: 'school'
+    }
+  ]
+});
+
+db.createCollection('courses');
+db.createCollection('grades');
+db.createCollection('materials');
+db.createCollection('students');
